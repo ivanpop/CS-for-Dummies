afed4a933116279db95411aedf360b20ca3c33d6
Ivan Popov <unfragable@data.bg>
2014-08-13 13:48:56 +0300

    Chapter 9 Solution 10

---
+++ Chapter 9 Solution 10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_9_Solution_10
+{
+    class Program
+    {
+
+        static long Factorial(long number)
+        {
+            if (number <= 1)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("{0}! = {1}", i, Factorial(i));              
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
