08072811d20894b7bafab55c921c13fbadd310e9
Ivan Popov <unfragable@data.bg>
2014-12-08 18:06:34 +0200

    Chapter 5 Solution 10

---
+++ Chapter 5 Solution 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter_5_Solution_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte points;
+            Console.Write("Enter points between 1 and 9: ");
+            points = Convert.ToByte(Console.ReadLine());
+            if (points >= 1 && points <= 3)
+                Console.WriteLine("Points multiplied by 10: {0}", points * 10);
+            else if (points >= 4 && points <= 6)
+                Console.WriteLine("Points multiplied by 100: {0}", points * 100);
+            else if (points >= 7 && points <= 9)
+                Console.WriteLine("Points multiplied by 1000: {0}", points * 1000);
+            else
+                Console.WriteLine("Wrong Input!");
+            Console.ReadLine();
+        }
+    }
+}
