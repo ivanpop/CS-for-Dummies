ed054741b96ebe9d27ab036c126ef9c0c17fd59d
Ivan Popov <unfragable@data.bg>
2014-12-09 21:52:09 +0200

    started Chapter 11 Solution 12

---
+++ Chapter 11 Solution 12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Chapter_11_Solution_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter equation: ");
+            string equation = Console.ReadLine();
+            string[] equationArr = equation.Split(' ');
+            string[] actions = new string[equationArr.Length];
+            string[] numbers = new string[equationArr.Length];
+            bool change = false;
+            
+            for(int i = 0; i < equationArr.Length; i++)
+            {
+                if (change) actions[i] = equationArr[i];
+                else numbers[i] = equationArr[i];
+                change = change ? false : true;                
+            }
+
+            numbers = numbers.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            actions = actions.Where(x => !string.IsNullOrEmpty(x)).ToArray();            
+            Console.ReadLine();
+        }
+    }
+}
