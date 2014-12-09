15db329da863691d6ec1698b51adb61487322f5c
Ivan Popov <unfragable@data.bg>
2014-12-09 20:00:38 +0200

    Chapter 11 Solution 10

---
+++ Chapter 11 Solution 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter_11_Solution_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.Write("Enter numbers: ");
+            string inputNumbers = Console.ReadLine();
+            string[] splitNumbers = inputNumbers.Split(' ');            
+
+            for (int i = 0; i < splitNumbers.Length; i++)
+            {
+                result += Convert.ToInt32(splitNumbers[i]);
+            }
+
+            Console.WriteLine("Result is: {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
