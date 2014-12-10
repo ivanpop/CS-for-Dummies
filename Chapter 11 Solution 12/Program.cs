6a8ef36b4405337f7717ef4a64af3ffbf4d56b21
Ivan Popov <unfragable@data.bg>
2014-12-10 14:16:46 +0200

    Solution 12 - added * and /

--- Chapter 11 Solution 12/Program.cs
+++ Chapter 11 Solution 12/Program.cs
@@ -5,24 +5,81 @@ namespace Chapter_11_Solution_12
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string equation;
+        public static string[] equationArr = new string[50];
+        public static string[] actions = new string[50];
+        public static string[] numbers = new string[50];
+        public static bool change = false;
+        public static double result = 0;
+
+        public static void removeNulls()
         {
+            numbers = numbers.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            actions = actions.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        }
+
+        static void Main(string[] args)
+        {            
             Console.WriteLine("Enter equation: ");
-            string equation = Console.ReadLine();
-            string[] equationArr = equation.Split(' ');
-            string[] actions = new string[equationArr.Length];
-            string[] numbers = new string[equationArr.Length];
-            bool change = false;
-            
+            equation = Console.ReadLine();
+            equationArr = equation.Split(' ');            
+
             for(int i = 0; i < equationArr.Length; i++)
             {
                 if (change) actions[i] = equationArr[i];
                 else numbers[i] = equationArr[i];
                 change = change ? false : true;                
             }
+            removeNulls();
 
-            numbers = numbers.Where(x => !string.IsNullOrEmpty(x)).ToArray();
-            actions = actions.Where(x => !string.IsNullOrEmpty(x)).ToArray();            
+            for (int i = 0; i < actions.Length; i++)
+            {   
+                if (i == 0)
+                {
+                    if (actions[i] == "*")
+                    {                        
+                        result += Convert.ToDouble(numbers[0]) * Convert.ToDouble(numbers[1]);
+                        numbers[0] = result.ToString();
+                        actions[i] = numbers[1] = null;
+                        removeNulls();
+                    }
+                    if (actions[i] == "/")
+                    {
+                        result += Convert.ToDouble(numbers[0]) / Convert.ToDouble(numbers[1]);
+                        numbers[0] = result.ToString();
+                        actions[i] = numbers[1] = null;
+                        removeNulls();
+                    }
+                }
+                else
+                {
+                    if (actions[i] == "*")
+                    {
+                        result += Convert.ToDouble(numbers[i]) * Convert.ToDouble(numbers[i + 1]);
+                        numbers[i] = result.ToString();
+                        actions[i] = numbers[i + 1] = null;
+                        removeNulls();
+                    }
+                    if (actions[i] == "/")
+                    {
+                        result += Convert.ToDouble(numbers[i]) / Convert.ToDouble(numbers[i + 1]);
+                        numbers[i] = result.ToString();
+                        actions[i] = numbers[i + 1] = null;
+                        removeNulls();
+                    }
+                }
+            }
+
+            for (int i = 0; i < actions.Length; i++)
+            {
+                Console.WriteLine(actions[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("Result = {0}", result);
             Console.ReadLine();
         }
     }
