c32d16edf565506d3fc930d91b0e95b328775ad0
Ivan Popov <unfragable@data.bg>
2014-12-10 18:20:19 +0200

    Solution 12 - '*','/','+' and '-' completed

--- Chapter 11 Solution 12/Program.cs
+++ Chapter 11 Solution 12/Program.cs
@@ -11,6 +11,7 @@ namespace Chapter_11_Solution_12
         public static string[] numbers = new string[50];
         public static bool change = false;
         public static double result = 0;
+        public static double temp = 0;
 
         public static void removeNulls()
         {
@@ -32,54 +33,93 @@ namespace Chapter_11_Solution_12
             }
             removeNulls();
 
-            for (int i = 0; i < actions.Length; i++)
-            {   
-                if (i == 0)
-                {
-                    if (actions[i] == "*")
-                    {                        
-                        result += Convert.ToDouble(numbers[0]) * Convert.ToDouble(numbers[1]);
-                        numbers[0] = result.ToString();
-                        actions[i] = numbers[1] = null;
-                        removeNulls();
-                    }
-                    if (actions[i] == "/")
+            do{
+                for (int i = 0; i < actions.Length; i++)
+                {   
+                    if (i == 0)
                     {
-                        result += Convert.ToDouble(numbers[0]) / Convert.ToDouble(numbers[1]);
-                        numbers[0] = result.ToString();
-                        actions[i] = numbers[1] = null;
-                        removeNulls();
+                        if (actions[i] == "*" || actions[i] == "/")
+                        {                        
+                            if (actions[i] == "*") temp = Convert.ToDouble(numbers[0]) * Convert.ToDouble(numbers[1]);
+                            else temp += Convert.ToDouble(numbers[0]) / Convert.ToDouble(numbers[1]);
+                            numbers[0] = temp.ToString();
+                            actions[i] = numbers[1] = null;
+                            temp = 0;
+                            removeNulls();
+                        }                    
+                    }
+                    else
+                    {
+                        int depth = 0;
+                        do
+                        {
+                            if (actions[i] == "*" || actions[i] == "/")
+                            {
+                                if (actions[i] == "*") temp = Convert.ToDouble(numbers[i]) * Convert.ToDouble(numbers[i + 1]);
+                                else temp += Convert.ToDouble(numbers[i]) / Convert.ToDouble(numbers[i + 1]);
+                                numbers[i] = temp.ToString();
+                                actions[i] = numbers[i + 1] = null;
+                                temp = 0;
+                                removeNulls();
+                                if (depth > 0) depth--;
+                            }
+                            foreach (var s in actions)
+                            {
+                                if (s == "*" || s == "/") depth++;
+                            }
+                        } while (depth != 0);
                     }
                 }
-                else
+
+                for (int i = 0; i < actions.Length; i++)
                 {
-                    if (actions[i] == "*")
+                    Console.WriteLine(actions[i]);
+                }
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+
+                Console.WriteLine("Continue {0} to +-", result);
+                Console.ReadLine();
+
+                for (int i = 0; i < actions.Length; i++)
+                {
+                    if (i == 0)
                     {
-                        result += Convert.ToDouble(numbers[i]) * Convert.ToDouble(numbers[i + 1]);
-                        numbers[i] = result.ToString();
-                        actions[i] = numbers[i + 1] = null;
-                        removeNulls();
+                        if (actions[i] == "+" || actions[i] == "-")
+                        {
+                            if (actions[i] == "+") numbers[0] = (Convert.ToDouble(numbers[0]) + Convert.ToDouble(numbers[1])).ToString();
+                            else numbers[0] = (Convert.ToDouble(numbers[0]) - Convert.ToDouble(numbers[1])).ToString();                            
+                            actions[i] = numbers[1] = null;
+                            removeNulls();
+                        }                    
                     }
-                    if (actions[i] == "/")
+                    else
                     {
-                        result += Convert.ToDouble(numbers[i]) / Convert.ToDouble(numbers[i + 1]);
-                        numbers[i] = result.ToString();
-                        actions[i] = numbers[i + 1] = null;
-                        removeNulls();
+                        if (actions[i] == "+" || actions[i] == "-")
+                        {
+                            if (actions[i] == "+") numbers[i] = (Convert.ToDouble(numbers[i]) + Convert.ToDouble(numbers[i + 1])).ToString();
+                            else numbers[i] = (Convert.ToDouble(numbers[i]) - Convert.ToDouble(numbers[i + 1])).ToString();                            
+                            actions[i] = numbers[i + 1] = null;
+                            removeNulls();
+                        }                    
                     }
                 }
-            }
 
-            for (int i = 0; i < actions.Length; i++)
-            {
-                Console.WriteLine(actions[i]);
-            }
+                for (int i = 0; i < actions.Length; i++)
+                {
+                    Console.WriteLine("actions {0}",actions[i]);
+                }
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.WriteLine(numbers[i]);
-            }
-            Console.WriteLine("Result = {0}", result);
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine("numbers {0}", numbers[i]);
+                }                
+            } while (numbers.Length > 1);
+
+            Console.WriteLine("Result is {0}", numbers);
             Console.ReadLine();
         }
     }
