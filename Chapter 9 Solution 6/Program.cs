b7fc1701513c7c71677e0b0b6652bff2c2a4eb5f
Ivan Popov <unfragable@data.bg>
2014-12-11 18:01:36 +0200

    Chapter 9 Solution 6

---
+++ Chapter 9 Solution 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Chapter_9_Solution_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool numberFound = false;
+            int[] arr = new int[] { 5, 5, 8, 6, 3, 2, 1, 4, 5, 8, 57, 5, 2, 1, 4, 5, 6, 8, 7, 5, 2 };
+            
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if(arr[i] > arr[i - 1] + arr[i + 1])
+                {
+                    Console.WriteLine(arr[i]);
+                    numberFound = true;
+                    break;
+                }
+            }
+            if (!numberFound) Console.WriteLine(-1);
+            Console.ReadLine();
+        }
+    }
+}
