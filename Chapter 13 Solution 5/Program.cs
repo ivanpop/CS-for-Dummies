2d98daa258e2b18a0371e3695aa1f41e81ec6524
Ivan Popov <unfragable@data.bg>
2014-12-12 22:47:04 +0200

    Chapter 13 Solution 5

---
+++ Chapter 13 Solution 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Chapter_13_Solution_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "We are living in a yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out in 5 days.";
+            Console.WriteLine(s);
+            s = s.ToLower();
+            Console.Write("\nEnter substring to search: ");
+            string substr = Console.ReadLine();
+            substr = substr.ToLower();
+            int count = Regex.Matches(s, substr).Count;
+            Console.WriteLine("{0} is found {1} times in the text.", substr, count);
+            Console.ReadLine();
+        }
+    }
+}
