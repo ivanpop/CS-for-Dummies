4171eb048bb03303136c67e348e2430901e4dde6
Ivan Popov <unfragable@data.bg>
2014-12-23 18:36:32 +0200

    Chapter 3 Solution 11

---
+++ Chapter 3 Solution 11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_3_Solution_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 35, p = 6, i = 1, mask = i << p;
+            Console.WriteLine((n & mask) != 0 ? "Third bit is 1" : "Third bit is 0");            
+        }
+    }
+}
