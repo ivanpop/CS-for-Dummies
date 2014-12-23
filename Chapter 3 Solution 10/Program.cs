f41eaadc072c46f88a17d3bee1fe7b8c2d57c3b4
Ivan Popov <unfragable@data.bg>
2014-12-23 18:34:06 +0200

    Chapter 3 Solution 10

---
+++ Chapter 3 Solution 10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_3_Solution_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int a = number / 1000;
+            int b = (number / 100) % 10;
+            int c = (number / 10) % 10;
+            int d = number % 10;
+
+            Console.WriteLine("1.Sum of digits = {0}", a + b + c + d);
+            Console.WriteLine("2.Digits backwards = {3}{2}{1}{0}", a, b, c, d);
+            Console.WriteLine("3.Last digit on first place = {3}{0}{1}{2}", a, b, c, d);
+            Console.WriteLine("4.Replace third and second digit = {0}{2}{1}{3}", a, b, c, d);
+        }
+    }
+}
