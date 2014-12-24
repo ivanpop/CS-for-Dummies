b35b4abfd1e0a6b21942f7771311272f361f180e
Ivan Popov <unfragable@data.bg>
2014-12-24 16:14:09 +0200

    Chapter 4 Solution 7

---
+++ Chapter 4 Solution 7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_4_Solution_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            bool parseSucceed = false;
+
+            do
+            {
+                Console.Write("Enter first number");
+                parseSucceed = Int32.TryParse(Console.ReadLine(), out a);
+                Console.WriteLine(parseSucceed);
+            } while (!parseSucceed);
+
+            do
+            {
+                Console.Write("Enter second number");
+                parseSucceed = Int32.TryParse(Console.ReadLine(), out b);
+                Console.WriteLine(parseSucceed);
+            } while (!parseSucceed);
+
+            do
+            {
+                Console.Write("Enter third number");
+                parseSucceed = Int32.TryParse(Console.ReadLine(), out c);
+                Console.WriteLine(parseSucceed);
+            } while (!parseSucceed);
+
+            do
+            {
+                Console.Write("Enter fourth number");
+                parseSucceed = Int32.TryParse(Console.ReadLine(), out d);
+                Console.WriteLine(parseSucceed);
+            } while (!parseSucceed);
+
+            do
+            {
+                Console.Write("Enter fifth number");
+                parseSucceed = Int32.TryParse(Console.ReadLine(), out e);
+                Console.WriteLine(parseSucceed);
+            } while (!parseSucceed);
+        }
+    }
+}
