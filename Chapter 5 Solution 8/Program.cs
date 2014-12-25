7f294135979b64deec359997a861543148644649
Ivan Popov <unfragable@data.bg>
2014-12-25 20:23:00 +0200

    Chapter 5 Solution 8

---
+++ Chapter 5 Solution 8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_5_Solution_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter variable type (0 - int, 1 - double, 2 string): ");
+            int intVar = Int32.Parse(Console.ReadLine());
+
+            switch (intVar)
+            {
+                case 0:
+                    {
+                        Console.Write("Enter int variable: ");
+                        intVar = Int32.Parse(Console.ReadLine());
+                        intVar++;
+                        Console.WriteLine("Int variable +1 = {0}", intVar);
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.Write("Enter double variable: ");
+                        double doubleVar = double.Parse(Console.ReadLine());
+                        doubleVar++;
+                        Console.WriteLine("Double variable +1 = {0}", doubleVar);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("Enter string variable: ");
+                        string stringVar = Console.ReadLine();
+                        stringVar = stringVar + '*';
+                        Console.WriteLine("String variable +* = {0}", stringVar);
+                        break;
+                    }
+                default: Console.WriteLine("Wrong input"); break;
+            }
+        }
+    }
+}
