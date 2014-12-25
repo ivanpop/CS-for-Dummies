135e6fd73f169a69a0bc95ed518ac1cb4ded9829
Ivan Popov <unfragable@data.bg>
2014-12-25 19:38:41 +0200

    Chapter 5 Solution 4

---
+++ Chapter 5 Solution 4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_5_Solution_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    a = a + c;
+                    c = a - c;
+                    a = a - c;
+                }
+                else if (a >= c)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }                
+            }
+            else if (a == b)
+            {
+                if (a < c)
+                {
+                    a = a + c;
+                    c = a - c;
+                    a = a - c;
+                }
+            }
+            else
+            {
+                if (b < c)
+                {
+                    b = b + c;
+                    c = b - c;
+                    b = b - c;
+                }
+                if (a < b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+            }
+            Console.WriteLine("{0}, {1}, {2}", a ,b ,c);
+        }
+    }
+}
---
+++ Chapter 5 Solution 4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Chapter 5 Solution 4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chapter 5 Solution 4")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c440487e-fa91-4b1e-9a2e-f4f74a0e5f7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
