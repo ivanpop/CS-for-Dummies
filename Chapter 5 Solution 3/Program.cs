2c501eed6209b253c4877f448f5a1819bb09a2db
Ivan Popov <unfragable@data.bg>
2014-12-25 19:09:08 +0200

    Chapter 5 Solution 3

---
+++ Chapter 5 Solution 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_5_Solution_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (a > b)
+                if (a > c) Console.WriteLine("A is the biggest");
+                else if (a < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("A and C are the biggest");
+            else if (a < b)
+                if (b > c) Console.WriteLine("B is the biggest");
+                else if (b < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("B and C are the biggest");
+            else if (a == b)
+                if (a == c) Console.WriteLine("All are equal");
+                else if (a < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("A and B are the biggest");
+        }
+    }
+}
