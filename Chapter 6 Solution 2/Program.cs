26dd20b205cc347e71f312797d4de7d505fc68b7
Ivan Popov <unfragable@data.bg>
2014-12-28 20:22:24 +0200

    Chapter 6 Solution 2

---
+++ Chapter 6 Solution 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_6_Solution_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < length; i++)
+            {
+                if (i % (3 * 7) != 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
