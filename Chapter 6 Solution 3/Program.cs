824b0523c692cdadb6e4d93aae0f8dbfb28ee220
Ivan Popov <unfragable@data.bg>
2014-12-28 20:46:01 +0200

    Chapter 6 Solution 3

---
+++ Chapter 6 Solution 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_6_Solution_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowest = 0, highest = 0, input;
+
+            Console.Write("Enter numbers length: ");
+            int lenght = Int32.Parse(Console.ReadLine());            
+
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write("Enter number: ");
+                input = Int32.Parse(Console.ReadLine());
+                if (i == 0) lowest = highest = input;
+                else
+                {
+                    if (lowest > input) lowest = input;
+                    if (highest < input) highest = input;
+                }                
+            }
+            Console.WriteLine("Lowest - {0}, Highest - {1}", lowest, highest);
+        }
+    }
+}
