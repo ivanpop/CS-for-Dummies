22980aeea2ebade49a9ca615271af1636708bbca
Ivan Popov <unfragable@data.bg>
2014-12-28 21:04:35 +0200

    Chapter 6 Solution 4

---
+++ Chapter 6 Solution 4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_6_Solution_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if(i != 0) Console.WriteLine();
+
+                for (int j = 0; j < 13; j++)
+                {
+                    switch (i)
+                    {
+                        case 0: Console.Write("Hearts "); break;
+                        case 1: Console.Write("Diamonds "); break;
+                        case 2: Console.Write("Spades "); break;
+                        case 3: Console.Write("Clubs "); break;
+                    }
+                    switch(j)
+                    {
+                        case 0: Console.WriteLine("2"); break;
+                        case 1: Console.WriteLine("3"); break;
+                        case 2: Console.WriteLine("4"); break;
+                        case 3: Console.WriteLine("5"); break;
+                        case 4: Console.WriteLine("6"); break;
+                        case 5: Console.WriteLine("7"); break;
+                        case 6: Console.WriteLine("8"); break;
+                        case 7: Console.WriteLine("9"); break;
+                        case 8: Console.WriteLine("10"); break;
+                        case 9: Console.WriteLine("J"); break;
+                        case 10: Console.WriteLine("Q"); break;
+                        case 11: Console.WriteLine("K"); break;
+                        case 12: Console.WriteLine("A"); break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
