c51aa550a21f6daa7d734990ee6903ef648e56de
Ivan Popov <unfragable@data.bg>
2014-12-29 14:47:35 +0200

    Chapter 6 Solution 7

---
+++ Chapter 6 Solution 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_6_Solution_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: (1<K<N) ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter K: (1<K<N) ");
+            int k = Int32.Parse(Console.ReadLine());
+            int nMinusK = n - k;
+
+            for (int i = n - 1; i > 0; i--) n *= i;
+            for (int i = k - 1; i > 0; i--) k *= i;
+            for (int i = nMinusK - 1; i > 0; i--) nMinusK *= i;
+
+            Console.WriteLine("Result is {0}", n * k / nMinusK);
+        }
+    }
+}
