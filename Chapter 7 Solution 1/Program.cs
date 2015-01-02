8550440490d9276aab1fafbab9385ed70142cec7
Ivan Popov <unfragable@data.bg>
2015-01-02 20:55:34 +0200

    Chapter 7 Solution 1

---
+++ Chapter 7 Solution 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
