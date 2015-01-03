838d831a2490af6bb986344812a3c19638974543
Ivan Popov <unfragable@data.bg>
2015-01-03 16:26:28 +0200

    Chapter 7 Solution 2

---
+++ Chapter 7 Solution 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool arraysEqual = true;
+
+            Console.Write("Enter lenght of first array: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arrA = new int[length];
+
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                Console.Write("Enter element {0}: ", i);
+                arrA[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\nEnter lenght of second array: ");
+
+            if (length != Int32.Parse(Console.ReadLine())) Console.WriteLine("\nArrays have different lengths.");
+            else
+            {
+                int[] arrB = new int[length];
+
+                for (int i = 0; i < arrB.Length; i++)
+                {
+                    Console.Write("Enter element {0}: ", i);
+                    arrB[i] = Int32.Parse(Console.ReadLine());
+                }
+
+                for (int i = 0; i < arrA.Length; i++)
+                {
+                    if (arrA[i] != arrB[i])
+                    {
+                        Console.WriteLine("\nArrays are different.");
+                        arraysEqual = false;
+                        break;
+                    }
+                }
+
+                if (arraysEqual) Console.WriteLine("\nArrays are the same.");
+            }
+        }
+    }
+}
