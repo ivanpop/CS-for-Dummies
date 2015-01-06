3630eba48c32bc0cc5939dc34b5a7712b66fc962
Ivan Popov <unfragable@data.bg>
2015-01-06 21:26:46 +0200

    Chapter 7 Solution 8

---
+++ Chapter 7 Solution 8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, iMin, temp;
+
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            for (j = 0; j < length - 1; j++)
+            {
+                iMin = j;
+                
+                for (i = j + 1; i < length; i++) if (arr[i] < arr[iMin]) iMin = i;
+
+                if (iMin != j)
+                {
+                    temp = arr[j];
+                    arr[j] = arr[iMin];
+                    arr[iMin] = temp;
+                }
+            }
+
+            for (i = 0; i < length; i++) Console.Write("{0} ", arr[i]);
+        }
+    }
+}
