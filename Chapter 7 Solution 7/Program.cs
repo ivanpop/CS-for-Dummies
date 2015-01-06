ae3036946d08e18aaee3d1d305787b9736ede376
Ivan Popov <unfragable@data.bg>
2015-01-06 21:08:56 +0200

    Chapter 7 Solution 7

---
+++ Chapter 7 Solution 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter K (K < N): ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr, (a, b) => b.CompareTo(a));
+
+            for (int i = 0; i < k; i ++) sum += arr[i];
+
+            Console.WriteLine("\nBiggest sum is {0}", sum);
+        }
+    }
+}
