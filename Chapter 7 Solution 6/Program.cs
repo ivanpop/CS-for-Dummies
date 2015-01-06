ec4d9132471bc8bd55ecd2c8e730a51069d2785a
Ivan Popov <unfragable@data.bg>
2015-01-06 20:23:04 +0200

    Chapter 7 Solution 6

---
+++ Chapter 7 Solution 6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0, tempIndex, tempCounter;
+
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }            
+ 
+            for (int i = 0; i < length; i++)
+            {
+                int[] tempResult = new int[length];
+                tempIndex = tempCounter = 1;                
+                tempResult[0] = arr[i];
+
+                for (int j = i + 1; j < length; j++)
+			    {                
+				    if (arr[j] > tempResult[tempIndex - 1])
+                    {
+                        tempResult[tempIndex] = arr[j];
+                        tempIndex++;
+                        tempCounter++;
+                    }
+                    else if (tempIndex > 1 && arr[j] > tempResult[tempIndex - 2] && arr[j] < tempResult[tempIndex - 1]) tempResult[tempIndex - 1] = arr[j];
+                }
+
+                if (counter < tempCounter)
+                {
+                    counter = tempCounter;
+                    result = tempResult;
+                }
+            }
+
+            for (int i = 0; i < counter; i++) Console.Write("{0},", result[i]);
+        }
+    }
+}
