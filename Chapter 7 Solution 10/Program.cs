65830e7c8a44800a959df58bc039730eef7b7fd7
Ivan Popov <unfragable@data.bg>
2015-01-07 14:43:45 +0200

    Chapter 7 Solution 10

---
+++ Chapter 7 Solution 10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0, tempCounter = 1, foundNumber = 0;
+
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1]) tempCounter++;
+                else tempCounter = 1;
+                if (tempCounter > counter)
+                {
+                    counter = tempCounter;
+                    foundNumber = arr[i];
+                }
+            }
+
+            Console.WriteLine("{0} was found {1} times.", foundNumber, counter);
+        }
+    }
+}
