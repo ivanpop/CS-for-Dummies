9093bb0e3575000df6e4bd2a7a06211bd95dadf1
Ivan Popov <unfragable@data.bg>
2015-01-07 14:24:23 +0200

    Chapter 7 Solution 9

---
+++ Chapter 7 Solution 9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, tempSum;
+            
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                tempSum = arr[i];
+
+                for (int j = i + 1; j < length; j++)
+                {
+                    tempSum += arr[j];
+                    if (tempSum > sum) sum = tempSum;
+                }
+            }
+
+            Console.WriteLine("Result is {0}. ", sum);
+        }
+    }
+}
