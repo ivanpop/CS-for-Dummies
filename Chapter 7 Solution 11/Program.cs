17472c0b4c3e78d1272ff455fa6027bccf69e97a
Ivan Popov <unfragable@data.bg>
2015-01-08 15:44:16 +0200

    Chapter 7 Solution 11

---
+++ Chapter 7 Solution 11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempCount = 1, count = 1, number = 0;
+
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1]) tempCount++;
+                else tempCount = 1;
+
+                if (tempCount > count)
+                {
+                    count = tempCount;
+                    number = arr[i];
+                }
+            }
+
+            Console.WriteLine("Most frequent number is {0}. Found {1} times.", number, count);
+        }
+    }
+}
