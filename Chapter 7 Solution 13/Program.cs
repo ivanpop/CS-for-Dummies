a13d4f46ddccc5b6eef8fb746c885bce1ba5ae16
Ivan Popov <unfragable@data.bg>
2015-01-10 14:18:11 +0200

    Chapter 7 Solution 13

---
+++ Chapter 7 Solution 13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 0, col = 0, sum = -1000;
+
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter M: ");
+            int m = Int32.Parse(Console.ReadLine());
+
+            int[,] arr = new int[n, m];
+
+            for(int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("Arr [{0}][{1}] = ", i, j);
+                    arr[i, j] = Int32.Parse(Console.ReadLine());
+                }            
+
+            for (int tempRow = 0; tempRow < arr.Length - 2; tempRow++)
+                for (int tempCol = 0; tempCol < arr.GetLength(0) - 2; tempCol++)
+                {
+                    int tempSum = arr[row, col] + arr[row, col + 1] + arr[row, col + 2] +
+                        arr[row + 1, col] + arr[row + 1, col + 1] + arr[row + 1, col + 2] +
+                        arr[row + 2, col] + arr[row + 2, col + 1] + arr[row + 2, col + 2];
+
+                    if (tempSum > sum)
+                    {
+                        row = tempRow;
+                        col = tempCol;
+                        sum = tempSum;
+                    }
+                }
+
+            Console.WriteLine("Result");
+            Console.WriteLine("{0} {1} {2}", arr[row, col], arr[row, col + 1], arr[row, col + 2]);
+            Console.WriteLine("{0} {1} {2}", arr[row + 1, col], arr[row + 1, col + 1], arr[row + 1, col + 2]);
+            Console.WriteLine("{0} {1} {2}", arr[row + 2, col], arr[row + 2, col + 2], arr[row + 2, col + 2]);
+            Console.WriteLine("The maximum sum is {0}.", sum);
+        }
+    }
+}
