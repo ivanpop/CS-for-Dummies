21ffa5c610d3192f3757d77a22850fca14720c05
Ivan Popov <unfragable@data.bg>
2015-01-11 17:28:41 +0200

    Chapter 7 Solution 15

---
+++ Chapter 7 Solution 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.Write("Enter a word: ");
+            char[] word = (Console.ReadLine()).ToCharArray();
+
+            for (int i = 0; i < word.Length; i++)
+                for (int j = 0; j < alphabet.Length; j++)
+                    if (word[i] == alphabet[j]) Console.Write("{0} ", j);
+        }
+    }
+}
---
+++ Chapter 7 Solution 17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+
+        }
+    }
+}
