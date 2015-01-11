00cbe924f0fdb26b46c08fa9cda3879b74ed79b7
Ivan Popov <unfragable@data.bg>
2015-01-11 17:39:13 +0200

    Chapter 7 Solution 16

---
+++ Chapter 7 Solution 16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter searched number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            int index = Array.BinarySearch(arr, number);
+
+            if (index >= 0) Console.Write("Number is on {0} index.", index);
+            else Console.Write("Number wasn't found.");
+        }
+    }
+}
