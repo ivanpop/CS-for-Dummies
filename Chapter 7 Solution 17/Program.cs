0eb6f6aa9a2607573ef71300c777e89d95c341bf
Ivan Popov <unfragable@data.bg>
2015-01-11 18:13:50 +0200

    Chapter 7 Solution 17

--- Chapter 7 Solution 17/Program.cs
+++ Chapter 7 Solution 17/Program.cs
@@ -8,6 +8,46 @@ namespace Chapter_7_Solution_17
 {
     class Program
     {
+        static public void DoMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid]) temp[tmp_pos++] = numbers[left++];
+                else temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end) temp[tmp_pos++] = numbers[left++];
+
+            while (mid <= right) temp[tmp_pos++] = numbers[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        static public void MergeSort_Recursive(int[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, left, mid);
+                MergeSort_Recursive(numbers, (mid + 1), right);
+
+                DoMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Enter array length: ");
@@ -21,7 +61,9 @@ namespace Chapter_7_Solution_17
                 arr[i] = Int32.Parse(Console.ReadLine());
             }
 
+            MergeSort_Recursive(arr, 0, arr.Length - 1);
 
+            for (int i = 0; i < arr.Length; i++ ) Console.WriteLine(arr[i]);
         }
     }
 }
