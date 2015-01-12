1abe0619d99afb2cd0e750f9420b755c817f4069
Ivan Popov <unfragable@data.bg>
2015-01-12 19:24:11 +0200

    Chapter 7 Solution 23

---
+++ Chapter 7 Solution 23/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_23
+{
+    class Program
+    {
+        public static int n;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            n = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter K: ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[k];
+
+            recSolution(arr, 0);
+        }
+
+        static void recSolution(int[] array, int index)
+        {
+            if (index != array.Length)
+                for (int i = 1; i <= n; i++)
+                {
+                    array[index] = i;
+                    recSolution(array, index + 1);
+                }
+            else
+            {
+                for (int i = 0; i < array.Length; i++) Console.Write(array[i] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
