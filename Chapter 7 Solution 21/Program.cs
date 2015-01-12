cbc66c6b1897726f1320e1cd706f10e7abe68948
Ivan Popov <unfragable@data.bg>
2015-01-12 18:26:20 +0200

    Chapter 7 Solution 21

---
+++ Chapter 7 Solution 21/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7_Solution_21
+{
+    class Program
+    {
+        public static int[] findSolution(int[] a, bool[] filter, int index, int s, int size)
+        {
+            if (index < a.Length)
+            {
+                filter[index] = true;
+                int[] x = findSolution(a, filter, index + 1, s, size);
+
+                if (x.Length > 0) return x;
+                else
+                {
+                    filter[index] = false;
+                    return findSolution(a, filter, index + 1, s, size);
+                }
+            }
+            else
+            {
+                int sum = 0, count = 0;
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (filter[i])
+                    {
+                        sum += a[i];
+                        count++;
+                    }
+                }
+
+                int[] solution = new int[0];
+
+                if (sum == s && count == size)
+                {
+                    solution = new int[count];
+                    count = 0;
+
+                    for (int i = 0; i < a.Length; i++) if (filter[i]) solution[count++] = a[i];
+                }
+                return solution;
+            }
+        }       
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter K = ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}: ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }            
+
+            Console.Write("Sum of elements, s = ");
+            int s = int.Parse(Console.ReadLine());
+
+            int[] solution = findSolution(arr, new bool[arr.Length], 0, s, size);
+
+            Console.WriteLine("Your solution:");
+            for (int i = 0; i < solution.Length; i++) Console.Write(solution[i] + "; ");
+            Console.ReadLine();
+        }
+    }
+}
