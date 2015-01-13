bc02b3a0299d61df4b60b59de2d9c7eb49639198
Ivan Popov <unfragable@data.bg>
2015-01-13 19:24:46 +0200

    Chapter 8 Solution 13

---
+++ Chapter 8 Solution 13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_8_Solution_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s, d;
+
+            Console.Write("Enter N: ");
+            string n = Console.ReadLine();
+            
+            do
+            {
+                Console.Write("Enter S (S == 2, 8, 10 or 16): ");
+                s = Int32.Parse(Console.ReadLine());
+            } while (s != 2 && s != 8 && s != 10 && s != 16);
+
+            do
+            {
+                Console.Write("Enter D (D == 2, 8, 10 or 16): ");
+                d = Int32.Parse(Console.ReadLine());
+            } while (d != 2 && d != 8 && d != 10 && d != 16);
+
+            n = Convert.ToString(Convert.ToInt32(n, s), d);
+
+            Console.WriteLine("Result is {0}.", n);
+        }
+    }
+}
