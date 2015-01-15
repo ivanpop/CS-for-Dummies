a17da6d4c8265dc348711ffb68491bff017ccae4
Ivan Popov <unfragable@data.bg>
2015-01-15 19:47:14 +0200

    Chapter 8 Solution 10

---
+++ Chapter 8 Solution 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_8_Solution_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deci = 0;
+
+            Console.Write("Enter binary number: ");
+            string binary = Console.ReadLine();
+            int length = binary.Length;
+            int power = length - 1;
+
+            for (int i = 0; i < length; i++)
+            {
+                deci += (int)(int.Parse(binary[i].ToString()) * Math.Pow(2, power));
+                power--;
+            }
+
+            Console.WriteLine("Result is {0}.", deci);
+        }
+    }
+}
