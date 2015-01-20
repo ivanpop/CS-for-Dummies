0457d6590a52a73062ff8e5512e52431451345b1
Ivan Popov <unfragable@data.bg>
2015-01-20 19:12:56 +0200

    Chapter 9 Solution 4

---
+++ Chapter 9 Solution 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_9_Solution_4
+{
+    class Program
+    {
+        static int CountNumber(int number, int[] arr)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < arr.Length; i++) if (number == arr[i]) counter++;
+
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} was found {1} times.", number, CountNumber(number, arr));
+        }
+    }
+}
