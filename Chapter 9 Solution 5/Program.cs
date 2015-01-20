1fead0fb58c4ade1f3c084473a64cce6d0514518
Ivan Popov <unfragable@data.bg>
2015-01-20 19:07:05 +0200

    Chapter 9 Solution 5 updated

--- Chapter 9 Solution 5/Program.cs
+++ Chapter 9 Solution 5/Program.cs
@@ -4,17 +4,60 @@ namespace Chapter_9_Solution_5
 {
     class Program
     {
+        static void CompareNumber(int pos, int[] arr)
+        {
+            if (pos == 0)
+            {
+                if (arr[0] < arr[1]) Console.WriteLine("{0} is smaller than it's right.", arr[0]);
+                else if (arr[0] > arr[1]) Console.WriteLine("{0} is bigger than it's right.", arr[0]);
+                else Console.WriteLine("{0} is equal to it's right.", arr[0]);
+            }
+            else if (pos == arr.Length - 1)
+            {
+                if (arr[arr.Length - 1] < arr[arr.Length - 2]) Console.WriteLine("{0} is smaller than it's left.", arr[pos]);
+                else if (arr[arr.Length - 1] > arr[arr.Length - 2]) Console.WriteLine("{0} is bigger than it's left.", arr[pos]);
+                else Console.WriteLine("{0} is equal to it's left.", arr[pos]);
+            }
+            else
+            {
+                if (arr[pos] < arr[pos - 1])
+                {
+                    if (arr[pos] < arr[pos + 1]) Console.WriteLine("{0} is smaller than it's neighbours.", arr[pos]);
+                    else if (arr[pos] == arr[pos + 1]) Console.WriteLine("{0} is smaller than it's left and equal to it's right.", arr[pos]);
+                    else Console.WriteLine("{0} is smaller than it's left and bigger than it's right.", arr[pos]);
+                }
+                else if (arr[pos] == arr[pos - 1])
+                {
+                    if (arr[pos] < arr[pos + 1]) Console.WriteLine("{0} is euqal to it's left and smaller than it's right.", arr[pos]);
+                    else if (arr[pos] == arr[pos + 1]) Console.WriteLine("{0} is equal to it's neighbours.", arr[pos]);
+                    else Console.WriteLine("{0} is equal to it's left and bigger than it's right.", arr[pos]);
+                }
+                else
+                {
+                    if (arr[pos] < arr[pos + 1]) Console.WriteLine("{0} is bigger than it's left and smaller than it's right.", arr[pos]);
+                    else if (arr[pos] == arr[pos + 1]) Console.WriteLine("{0} is bigger than it's left and equal to it's right.", arr[pos]);
+                    else Console.WriteLine("{0} is bigger than it's neighbours.", arr[pos]);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 5, 5, 11, 6, 3, 2, 1, 4, 5, 8, 57, 5, 2, 1, 4, 5, 6, 8, 7, 5, 2, 3 };
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
 
-            foreach(var i in arr) Console.Write("{0} ", i);
-            Console.Write("\nEnter index between 1 and 20: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            if (arr[choice] > arr[choice - 1] + arr[choice +1]) Console.WriteLine("The number is bigger");
-            else if (arr[choice] < arr[choice - 1] + arr[choice +1]) Console.WriteLine("The number is smaller");
-            else Console.WriteLine("The number is equal");
-            Console.ReadLine();
+            int[] arr = new int[length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter position in array: ");
+            int pos = Int32.Parse(Console.ReadLine());
+
+            CompareNumber(pos, arr);
         }
     }
 }
