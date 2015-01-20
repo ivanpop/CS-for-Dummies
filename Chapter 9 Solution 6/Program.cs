f81bc1a6765aac1eaa28a75638f57121270777a4
Ivan Popov <unfragable@data.bg>
2015-01-20 19:08:25 +0200

    Chapter 9 Solution 6 updated

--- Chapter 9 Solution 6/Program.cs
+++ Chapter 9 Solution 6/Program.cs
@@ -4,22 +4,35 @@ namespace Chapter_9_Solution_6
 {
     class Program
     {
-        static void Main(string[] args)
+        static int number = int.MinValue;
+
+        static void CompareNumber(int[] arr)
         {
-            bool numberFound = false;
-            int[] arr = new int[] { 5, 5, 8, 6, 3, 2, 1, 4, 5, 8, 57, 5, 2, 1, 4, 5, 6, 8, 7, 5, 2 };
-            
             for (int i = 1; i < arr.Length - 1; i++)
-            {
-                if(arr[i] > arr[i - 1] + arr[i + 1])
+                if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
                 {
-                    Console.WriteLine(arr[i]);
-                    numberFound = true;
+                    number = arr[i];
                     break;
                 }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
             }
-            if (!numberFound) Console.WriteLine(-1);
-            Console.ReadLine();
+
+            CompareNumber(arr);
+
+            if (number == int.MinValue) Console.WriteLine("-1");
+            else Console.WriteLine("{0} is bigger than it's neighbours", number);
         }
     }
 }
