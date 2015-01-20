8929a52f6a9dd4764822c8775f08ca208bbd1b57
Ivan Popov <unfragable@data.bg>
2015-01-20 19:11:19 +0200

    Chapter 9 Solution 2

---
+++ Chapter 9 Solution 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_9_Solution_2
+{
+    class Program
+    {
+        static void GetMax(int first, int second)
+        {
+            if (first > second) a = first;
+            else a = second;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            c = Int32.Parse(Console.ReadLine());
+
+            GetMax(a, b);
+            GetMax(a, c);
+
+            Console.WriteLine("Biggest number is {0}", a);
+        }
+    }
+}
