35975247aa5368430f8a8c7d843f2e78f7a40fd2
Ivan Popov <unfragable@data.bg>
2015-02-04 21:07:07 +0200

    Chapter 10 Solution 2

---
+++ Chapter 10 Solution 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_10_Solution_2
+{
+    class Program
+    {
+        static void GetCombinations(int[] arr, int index, int start, int end)
+        {
+            if (index >= arr.Length)
+            {
+                Console.Write("(");
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write("{0} ", arr[i]);
+                    else Console.Write(arr[i]);
+                Console.Write("), ");
+            }
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    arr[index] = i;
+                    GetCombinations(arr, index + 1, i, end);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter K: ");
+            int k = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[k];
+            GetCombinations(arr, 0, 1, n);
+        }
+    }
+}
