fd08c3a0ee980c068d8efc73946169e03bfcf699
Ivan Popov <unfragable@data.bg>
2015-02-13 14:47:42 +0200

    Chapter 10 Solution 9

---
+++ Chapter 10 Solution 9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_10_Solution_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+
+            for(int i = 0; i < length; i++)
+            {
+                Console.Write("Enter {0} element: ", i + 1);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter sum: ");
+            int sum = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int first = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int second = arr[j];
+
+                    if ((first + second) == sum)
+                        Console.WriteLine("({0}, {1}) ", first, second);
+                }
+            }
+        }
+    }
+}
