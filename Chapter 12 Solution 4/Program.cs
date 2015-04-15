99c843f6fbaf1de1c86614b4bb41a558f5f2d3b3
Ivan Popov <unfragable@data.bg>
2015-04-15 15:08:17 +0300

    Chapter 12 Solution 4

---
+++ Chapter 12 Solution 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_12_Solution_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            int n = -1;
+            bool invalidNumber = false;
+
+            try
+            {
+                n = Convert.ToInt32(input);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid number!");
+                invalidNumber = true;
+            }
+            finally
+            {
+                if (n < 0)
+                {
+                    if (!invalidNumber) Console.WriteLine("Invalid number!");
+                }
+                else Console.WriteLine(Math.Sqrt(n));
+            }
+
+            Console.WriteLine("Good Bye");
+        }
+    }
+}
