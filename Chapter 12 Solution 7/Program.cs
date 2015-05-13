7bbeec38b02babe0e79e7249b10d7cf7aab4cabb
Ivan Popov <unfragable@data.bg>
2015-05-13 11:23:06 +0300

    Chapter 12 Solution 7

---
+++ Chapter 12 Solution 7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_12_Solution_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"C:\Users\Ivan\Desktop\text.txt"))
+                {
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("The specified path is invalid!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Invalid syntax!");
+            }
+        }
+    }
+}
