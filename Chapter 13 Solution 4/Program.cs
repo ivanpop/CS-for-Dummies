511cff71f6373cfb45d19d7834f2da8fb5730ee3
Ivan Popov <unfragable@data.bg>
2016-08-26 13:42:53 +0300

    26082016
    1. Added Chapter 13 Solution 4

---
+++ Chapter 13 Solution 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chapter_13_Solution_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "one\two\three";
+            string[] sArr = s.Split('\\');
+            foreach (string word in sArr) Console.WriteLine(word);            
+            Console.ReadLine();
+        }
+    }
+}
