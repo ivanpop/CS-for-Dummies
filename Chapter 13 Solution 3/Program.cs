49053946fdbfd5abc8af9297817587a56f569b49
Ivan Popov <unfragable@data.bg>
2016-08-26 13:23:27 +0300

    26082016
    1. Added Chapter 13 Solution 3

---
+++ Chapter 13 Solution 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chapter_13_Solution_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter equation: ");
+            string input = Console.ReadLine();
+            char[] inputArr = input.ToCharArray();
+            int counter = 0;
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                if (inputArr[i] == '(') counter++;
+                if (inputArr[i] == ')') counter--;
+                if (counter < 0) break;                
+            }
+
+            if (counter == 0) Console.WriteLine("Correct equation.");
+            else Console.WriteLine("Wrong equation!");
+            Console.ReadLine();
+        }
+    }
+}
