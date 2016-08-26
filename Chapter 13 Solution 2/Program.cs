33447ede4484f19b798a502b2a934fffbed5b4ab
Ivan Popov <unfragable@data.bg>
2016-08-26 12:13:08 +0300

    26082016
    1. Added Chapter 13 Solution 2

---
+++ Chapter 13 Solution 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Chapter_13_Solution_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text to reverse: ");
+            string input = Console.ReadLine();
+            string output = new string(input.ToCharArray().Reverse().ToArray());
+            Console.WriteLine("\"{0}\" reversed \"{1}\"", input, output);
+            Console.ReadLine();
+        }
+    }
+}
