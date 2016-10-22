25bc7af9bbc25efb5d274f9fff1b031ee76a5450
Ivan Popov <unfragable@data.bg>
2016-10-22 19:11:50 +0300

    Chapter 13 Solution 7

---
+++ Chapter 13 Solution 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Chapter_13_Solution_7
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("Input text (20 char max): ");
+            string text = Console.ReadLine();
+
+            while (text.Length > 20)
+            {
+                Console.Write("Input 20 char max!: ");
+                text = Console.ReadLine();
+            }
+
+            while (text.Length < 20) text += "*";
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
