b3c29228aa5a38c14378960de3ef101b60fa180d
Ivan Popov <unfragable@data.bg>
2016-10-22 23:12:16 +0300

    Chapter 13 Solution 9

---
+++ Chapter 13 Solution 8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chapter_13_Solution_8
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("Input text: ");
+            string text = Console.ReadLine();
+            foreach (char c in text) Console.Write("\\u{0:x4}", ((int)c));
+            Console.ReadLine();
+        }
+    }
+}
