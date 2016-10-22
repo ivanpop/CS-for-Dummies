6e44a54a1332f2243ac3992d43c1a46c568ee732
Ivan Popov <unfragable@data.bg>
2016-10-23 00:04:41 +0300

    Chapter 13 Solution 10

---
+++ Chapter 13 Solution 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter_13_Solution_10
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            string text = "Microsoft announced its next generation C# compiler today. It uses advanced parser and special optimizer for the Microsoft CLR.", forbiddenWords = "C#,CLR,Microsoft";
+            string[] forbiddenWordsArr = forbiddenWords.Split(',');
+            string[] censoredForbiddenWordsArr = new string[forbiddenWordsArr.Length];
+
+            for (int i = 0; i < forbiddenWordsArr.Length; i++)
+                censoredForbiddenWordsArr[i] = new string('*', forbiddenWordsArr[i].Length);
+
+            for (int i = 0; i < forbiddenWordsArr.Length; i++)
+               text = text.Replace(forbiddenWordsArr[i], censoredForbiddenWordsArr[i]);   
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
