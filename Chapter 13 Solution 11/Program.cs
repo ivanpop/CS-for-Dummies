c038c4d6a94fb19e5e5207381ed02049c116a89a
Ivan Popov <unfragable@data.bg>
2016-10-23 13:16:55 +0300

    Chapter 13 Solution 11

---
+++ Chapter 13 Solution 11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter_13_Solution_11
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("Input number: ");
+            int number = System.Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0, -15} {1, 15}", "Decimal:", number);
+            Console.WriteLine("{0, -15} {1, 15}", "Hexadecimal:", number.ToString("X"));
+            Console.WriteLine("{0, -15} {1, 15}", "Currency:", string.Format("{0:C}", number));
+            Console.WriteLine("{0, -15} {1, 15}", "Percent:", string.Format("{0:P2}", number));
+            Console.WriteLine("{0, -15} {1, 15}", "Scientific:", number.ToString("\\0.#####E0"));
+            Console.ReadLine();
+        }
+    }
+}
