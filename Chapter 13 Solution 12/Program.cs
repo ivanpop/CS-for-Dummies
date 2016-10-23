b9a8122c304031ce76ac5aed27382c2ad92149d2
Ivan Popov <unfragable@data.bg>
2016-10-23 14:52:43 +0300

    Chapter 13 Solution 12

---
+++ Chapter 13 Solution 12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chapter_13_Solution_12
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("Input HTML: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("[protocol] = \"{0}\"", text.Substring(0, text.IndexOf("://")));
+            Console.WriteLine("[server] = \"{0}\"", text.Substring(text.IndexOf("://") + 3).Split('/'));      
+            Console.WriteLine("[resource] = \"{0}\"", text.Substring(text.IndexOf("://") + 3).Substring(text.Substring(text.IndexOf("://") + 3).IndexOf('/')));
+
+            Console.ReadLine();
+        }
+    }
+}
