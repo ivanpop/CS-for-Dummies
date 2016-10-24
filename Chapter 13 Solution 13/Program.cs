18177cfa870075a454e27aec1e472f6b4044424c
Ivan Popov <unfragable@data.bg>
2016-10-24 22:37:56 +0300

    Chapter 13 Solution 13

---
+++ Chapter 13 Solution 13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter_13_Solution_13
+{
+    public class Program
+    {
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public void Main(string[] args)
+        {
+            string text = "C# is not C++ and PHP is not Delphi";
+
+            text = Reverse(text);
+
+            string[] textArr = text.Split(' ');
+
+            text = "";
+
+            foreach (string s in textArr) text += Reverse(s) + ' ';
+            
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
