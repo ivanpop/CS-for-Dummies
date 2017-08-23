b11c4f845f88e9e5115568c4040fafbb5b1c27b8
Ivan Popov <unfragable@data.bg>
2017-08-23 12:25:01 +0300

    23/08/2017
    Added Chapter 13 Solution 15

---
+++ Chapter 13 Solution 15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter_13_Solution_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "<p>Please visit <a href=\"http://softuni.bg\">our site</a> to choose a software engineering training course. Also visit<a href=\"http://softuni.bg" + "/forum\">our forum</a> to discuss the courses.</ p >";
+            text = text.Replace("<a href=\"", "[URL=");
+            text = text.Replace("\">", "]");
+            text = text.Replace("</a>", "[/URL]");
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
