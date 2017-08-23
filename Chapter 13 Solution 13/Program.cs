51a99ce1d89476620e811ad900ae6b7533df1661
Ivan Popov <unfragable@data.bg>
2017-08-23 08:38:42 +0300

    23/08/2017
    Added Chapter 13 Solution 14

--- Chapter 13 Solution 13/Program.cs
+++ Chapter 13 Solution 13/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Chapter_13_Solution_13
 {
---
+++ Chapter 13 Solution 14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter_13_Solution_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dictionary = ".NET - platform for applications from Microsoft\nCLR - managed execution environment for .NET\nnamespace - hierarchical organization of classes\n";            string[] tempDictionary = new string[10];            tempDictionary = dictionary.Split(new string[] { "\n" }, StringSplitOptions.None);            Console.Write("Enter word: ");            string input = Console.ReadLine() + " - ";                              
+            foreach (string str in tempDictionary)
+            {
+                if (str.Contains(input)) Console.WriteLine(str.Substring(input.Length));
+            }
+                Console.ReadLine();
+        }
+    }
+}
