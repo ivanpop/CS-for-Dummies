cbb3f9aef1e375e43e45964a609854b9e48d4a56
Bennebilimamk <33234594+Bennebilimamk@users.noreply.github.com>
2017-10-30 21:49:20 +0200

    Error in bit positions
    Bit positions you want to change should be (bitpos-1)... or else when you are trying to change 3rd bit you actually change the 4th

--- Chapter 3 Solution 15/Program.cs
+++ Chapter 3 Solution 15/Program.cs
@@ -12,25 +12,25 @@ namespace Chapter_3_Solution_15
         {
             Console.Write("Enter number: ");
             int v = Convert.ToInt32(Console.ReadLine());
-            int mask = 1 << 3;
+            int mask = 1 << 2;
             int bitAt3 = (v & mask) != 0 ? 1 : 0;
-            mask = 1 << 4;
+            mask = 1 << 3;
             int bitAt4 = (v & mask) != 0 ? 1 : 0;
-            mask = 1 << 5;
+            mask = 1 << 4;
             int bitAt5 = (v & mask) != 0 ? 1 : 0;
-            mask = 1 << 24;
+            mask = 1 << 23;
             int bitAt24 = (v & mask) != 0 ? 1 : 0;
-            mask = 1 << 25;
+            mask = 1 << 24;
             int bitAt25 = (v & mask) != 0 ? 1 : 0;
-            mask = 1 << 26;
+            mask = 1 << 25;
             int bitAt26 = (v & mask) != 0 ? 1 : 0;
 
-            v = (bitAt3 == 0) ? v = v & (~(1 << 24)) : v = v | (1 << 24);
-            v = (bitAt4 == 0) ? v = v & (~(1 << 25)) : v = v | (1 << 25);
-            v = (bitAt5 == 0) ? v = v & (~(1 << 26)) : v = v | (1 << 26);
-            v = (bitAt24 == 0) ? v = v & (~(1 << 3)) : v = v | (1 << 3);
-            v = (bitAt25 == 0) ? v = v & (~(1 << 4)) : v = v | (1 << 4);
-            v = (bitAt26 == 0) ? v = v & (~(1 << 5)) : v = v | (1 << 5);
+            v = (bitAt3 == 0) ? v = v & (~(1 << 23)) : v = v | (1 << 23);
+            v = (bitAt4 == 0) ? v = v & (~(1 << 24)) : v = v | (1 << 24);
+            v = (bitAt5 == 0) ? v = v & (~(1 << 25)) : v = v | (1 << 25);
+            v = (bitAt24 == 0) ? v = v & (~(1 << 2)) : v = v | (1 << 2);
+            v = (bitAt25 == 0) ? v = v & (~(1 << 3)) : v = v | (1 << 3);
+            v = (bitAt26 == 0) ? v = v & (~(1 << 4)) : v = v | (1 << 4);
 
             Console.WriteLine(v);
         }
