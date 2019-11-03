37ed0d2f63ac25eca70c69ea73d86436cd30788a
Ivan Popov <unfragablegaming@gmail.com>
2019-11-03 12:40:27 +0200

    03112019-2
    Optimised Chapter 13 Solution 3 by removing char[] inputArr.

--- Chapter 13 Solution 3/Program.cs
+++ Chapter 13 Solution 3/Program.cs
@@ -8,18 +8,18 @@ namespace Chapter_13_Solution_3
         {
             Console.Write("Enter equation: ");
             string input = Console.ReadLine();
-            char[] inputArr = input.ToCharArray();
             int counter = 0;
 
-            for (int i = 0; i < inputArr.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                if (inputArr[i] == '(') counter++;
-                if (inputArr[i] == ')') counter--;
+                if (input[i] == '(') counter++;
+                if (input[i] == ')') counter--;
                 if (counter < 0) break;                
             }
 
             if (counter == 0) Console.WriteLine("Correct equation.");
             else Console.WriteLine("Wrong equation!");
+
             Console.ReadLine();
         }
     }
