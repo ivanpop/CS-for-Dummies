22917d89982b713ad1cd3f94c4e18b2f267869f6
Ivan Popov <unfragablegaming@gmail.com>
2019-11-03 13:56:28 +0200

    03112019-4
    Changed Chapter 13 Solution 5

--- Chapter 13 Solution 5/Program.cs
+++ Chapter 13 Solution 5/Program.cs
@@ -7,14 +7,29 @@ namespace Chapter_13_Solution_5
     {
         static void Main(string[] args)
         {
-            string s = "We are living in a yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out in 5 days.";
-            Console.WriteLine(s);
-            s = s.ToLower();
+            int counter = 0;
+            string str = "We are living in a yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
+            Console.WriteLine(str);
+            str = str.ToLower();
             Console.Write("\nEnter substring to search: ");
-            string substr = Console.ReadLine();
-            substr = substr.ToLower();
-            int count = Regex.Matches(s, substr).Count;
-            Console.WriteLine("{0} is found {1} times in the text.", substr, count);
+            string substr = Console.ReadLine().ToLower();
+
+            int i = str.IndexOf(substr);
+
+            if (i != -1)
+            {
+                counter++;
+
+                while (i != -1 && counter >= 1)
+                {
+                    i = str.IndexOf(substr, i + substr.Length);
+                    counter++;
+                }
+
+                counter--;
+            }
+
+            Console.WriteLine("'{0}' is found {1} times in the text.", substr, counter);
             Console.ReadLine();
         }
     }
