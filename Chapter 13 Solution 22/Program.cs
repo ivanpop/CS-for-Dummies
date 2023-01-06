d9ed2e3547d94050cbf08a1c70a90da80f959efd
Ivan Popov <unfragablegaming@gmail.com>
2023-01-06 23:17:19 +0200

    Added Chapter 13 Solution 22

---
+++ Chapter 13 Solution 22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chapter_13_Solution_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+
+            char[] arr = new char[65536];
+
+            foreach (char c in input)
+                arr[c]++;
+
+            for (int i = 0; i < 65536; i++)
+                if (arr[i] > 0)
+                    Console.WriteLine($"{(char)i} - {(int)arr[i]}");
+        }
+    }
+}
