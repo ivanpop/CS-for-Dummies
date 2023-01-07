6186b121fa375f2d395a9061a73fc35900056eb7
Ivan Popov <unfragablegaming@gmail.com>
2023-01-08 00:21:59 +0200

    Added Chapter 13 Solution 24

---
+++ Chapter 13 Solution 24/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Chapter_13_Solution_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length - 1; i++)
+                if (input[i + 1] != input[i])
+                    sb.Append(input[i]);
+
+            sb.Append(input[input.Length - 1]);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
