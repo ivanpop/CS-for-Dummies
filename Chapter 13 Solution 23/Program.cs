7b81d6837e41e67f49a7f4b0dae25204f5eaa4d6
Ivan Popov <unfragablegaming@gmail.com>
2023-01-07 12:07:26 +0200

    Added Chapter 13 Solution 23

---
+++ Chapter 13 Solution 23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter_13_Solution_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+            
+            string[] textArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            SortedDictionary<string, int> dict = new SortedDictionary<string, int>();
+
+            foreach (var word in textArray)
+            {
+                if (!dict.ContainsKey(word))
+                    dict.Add(word, 0);
+
+                dict[word]++;
+            }
+
+            foreach (var word in dict)
+                Console.WriteLine($"{word.Key} - {word.Value}");
+        }
+    }
+}
