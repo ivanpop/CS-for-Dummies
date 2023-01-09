3284c050381762a70dd8e106d0f18f6394be587a
Ivan Popov <unfragablegaming@gmail.com>
2023-01-09 21:45:07 +0200

    Added Chapter 13 Solution 25

---
+++ Chapter 13 Solution 25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter_13_Solution_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Sort(words);
+
+            foreach (var word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
