cdab9aa0a1cead2d9a1d0ac020be91d7a179fe88
Ivan Popov <unfragablegaming@gmail.com>
2023-01-11 23:12:31 +0200

    Added Chapter 14 Solution 1

---
+++ Chapter 14 Solution 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter_14_Solution_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Chapter 14 Solution 1/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace Chapter_14_Solution_1
+{
+    internal class Student
+    {
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public enum speciality
+        {
+            CSharp,
+            Java,
+            Python
+        }
+        public enum university 
+        {
+            Plovdiv_University,
+            Sofia_University,
+            Varna_University
+        }
+        public string email { get; set; }
+        public string mobileNumber { get; set; }
+    }
+}
